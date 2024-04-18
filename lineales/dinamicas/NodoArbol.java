79e9f803ec3952444ba552e3b189565d590e56cd
Juan <5d0f2g9h@gmail.com>
2024-04-17 23:55:51 -0300

    Agregada clase NodoArbol al paquete lineales.dinamicas

---
+++ lineales/dinamicas/NodoArbol.java
@@ -0,0 +1,37 @@
+package lineales.dinamicas;
+
+public class NodoArbol {
+    private Object elem;
+    private NodoArbol izquierdo;
+    private NodoArbol derecho;
+
+    public NodoArbol(Object elem, NodoArbol izquierdo, NodoArbol derecho) {
+        this.elem = elem;
+        this.izquierdo = izquierdo;
+        this.derecho = derecho;
+    }
+
+    public Object getElemento() {
+        return this.elem;
+    }
+
+    public NodoArbol getIzquierdo() {
+        return this.izquierdo;
+    }
+
+    public NodoArbol getDerecho() {
+        return this.derecho;
+    }
+
+    public void setElemento(Object elem) {
+        this.elem = elem;
+    }
+
+    public void setIzquierdo(NodoArbol izquierdo) {
+        this.izquierdo = izquierdo;
+    }
+
+    public void setDerecho(NodoArbol derecho) {
+        this.derecho = derecho;
+    }
+}
